7c59ceb6d14fa1ef73af262bb889927494ac12f8
shohan ahmed <shohanur7777@gmail.com>
2021-06-10 12:14:55 +0600

    add a RectTailwindcss Cli

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <HelloWorld></HelloWorld>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function HelloWorld(){
+    return(
+        <div className="mt-40 ">
+            <h1 duration="down" className="bg-green-500 p-8 text-3xl text-white">Hello World</h1>
+        </div>
+    )
+}
+export default HelloWorld
